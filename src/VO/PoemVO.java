6bbae79049118948d8d765f2cc4c5d9e9af0f6f5
macarize <54901021+macarize@users.noreply.github.com>
2020-06-26 22:44:33 +0900

    add admin

--- src/VO/PoemVO.java
+++ src/VO/PoemVO.java
@@ -16,6 +16,48 @@ public class PoemVO {
 	private int uno;
 	private Timestamp time;
 	private int report;
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public int getLike() {
+		return like;
+	}
+	public void setLike(int like) {
+		this.like = like;
+	}
+	public int getUno() {
+		return uno;
+	}
+	public void setUno(int uno) {
+		this.uno = uno;
+	}
+	public Timestamp getTime() {
+		return time;
+	}
+	public void setTime(Timestamp time) {
+		this.time = time;
+	}
+	public int getReport() {
+		return report;
+	}
+	public void setReport(int report) {
+		this.report = report;
+	}
 	
 	
 }
